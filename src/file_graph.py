0c5aa4a0e076ce4e59a2600dd27a174dc769cb0f
Freya Pines <one-true-cat@yandex.ru>
2025-12-02 23:29:40 +0300

    done task

--- src/file_graph.py
+++ src/file_graph.py
@@ -1,18 +1,20 @@
 from typing import Generator
 import networkx as nx
+import matplotlib.pyplot as plt
 
 from src.models import Case
 
 
 class FileGraph:
-    def __init__(self) -> None:
+    def __init__(self, edge_len: int = 1, graph_cls = nx.Graph) -> None:
         self.cases: list[Case] = []
-        self._graph: nx.Graph[str] = nx.Graph()
+        self._graph: nx.Graph[str] = graph_cls()
+        self.edge_len = edge_len
 
     def add_case(self, case: Case) -> None:
         self.cases.append(case)
-        self._graph.add_nodes_from([case.guilty, case.victim])
-        self._graph.add_edge(case.guilty, case.victim)
+        self._graph.add_nodes_from([case.guilty_initials, case.victim_initials])
+        self._graph.add_edge(case.guilty_initials, case.victim_initials , length=self.edge_len)
 
     def add_cases(self, cases: list[Case]) -> None:
         for case in cases:
@@ -26,5 +28,5 @@ class FileGraph:
     def components_count(self) -> int:
         return len(self.components)
 
-    def draw(self) -> None:
-        nx.draw(self._graph, with_labels=True)
+    def draw(self, *args, **kwargs) -> None:
+        nx.draw(self._graph, *args, with_labels=True, **kwargs)
--- src/models.py
+++ src/models.py
@@ -4,3 +4,16 @@ class Case(BaseModel):
     num_claims: int
     guilty: str
     victim: str
+
+    @staticmethod
+    def __get_short_name(full_name: str) -> str:
+        surname, name, patr = full_name.split()
+        return f"{surname} {name[0].capitalize()}. {patr[0].capitalize()}."
+    
+    @property
+    def guilty_initials(self):
+        return self.__get_short_name(self.guilty)
+    
+    @property
+    def victim_initials(self):
+        return self.__get_short_name(self.victim)
